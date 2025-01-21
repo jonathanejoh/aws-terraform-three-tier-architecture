20c5ffaa8ce5c79ac828b3a865f9fbc26a87d72a
jonathanejoh <ejohjonathan7@gmail.com>
2025-01-21 02:10:33 -0500

    update terraform config

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,24 +1,24 @@
 # Create an EC2 Auto Scaling Group - web
 resource "aws_autoscaling_group" "ejoh-three-tier-web-asg" {
-  name                 = "ejoh-three-tier-web-asg"
-  launch_configuration = aws_launch_template.ejoh-three-tier-web-lconfig.id
-  vpc_zone_identifier  = [aws_subnet.ejoh-three-tier-pub-sub-1.id, aws_subnet.ejoh-three-tier-pub-sub-2.id]
-  min_size             = 2
-  max_size             = 3
-  desired_capacity     = 2
-  depends_on           = [aws_launch_template.ejoh-three-tier-web-lconfig]
+  name                = "ejoh-three-tier-web-asg"
+  launch_template     {id = aws_launch_template.ejoh-three-tier-web-lconfig.id }
+  vpc_zone_identifier = [aws_subnet.ejoh-three-tier-pub-sub-1.id, aws_subnet.ejoh-three-tier-pub-sub-2.id]
+  min_size            = 2
+  max_size            = 3
+  desired_capacity    = 2
+  depends_on          = [aws_launch_template.ejoh-three-tier-web-lconfig]
 }
 
 # Create an EC2 Auto Scaling Group - app
 resource "aws_autoscaling_group" "ejoh-three-tier-app-asg" {
-  name                 = "ejoh-three-tier-app-asg"
-  launch_configuration = aws_launch_template.ejoh-three-tier-app-lconfig.id
-  vpc_zone_identifier  = [aws_subnet.ejoh-three-tier-pvt-sub-1.id, aws_subnet.ejoh-three-tier-pvt-sub-2.id]
-  min_size             = 2
-  max_size             = 3
-  desired_capacity     = 2
+  name                = "ejoh-three-tier-app-asg"
+  launch_template     {id = aws_launch_template.ejoh-three-tier-app-lconfig.id}
+  vpc_zone_identifier = [aws_subnet.ejoh-three-tier-pvt-sub-1.id, aws_subnet.ejoh-three-tier-pvt-sub-2.id]
+  min_size            = 2
+  max_size            = 3
+  desired_capacity    = 2
 
-depends_on           = [aws_launch_template.ejoh-three-tier-app-lconfig]
+  depends_on = [aws_launch_template.ejoh-three-tier-app-lconfig]
 }
 
 ###################################################################################################################################
@@ -28,39 +28,38 @@ depends_on           = [aws_launch_template.ejoh-three-tier-app-lconfig]
 resource "aws_launch_template" "ejoh-three-tier-web-lconfig" {
   name          = "wjoh-three-tier-web-lconfig"
   image_id      = "ami-0866a3c8686eaeeba"
-  instance_type   = "t2.micro"
-  key_name        = "medium"
+  instance_type = "t2.micro"
+  key_name      = "medium"
   user_data     = base64encode(file("${path.module}/ejohdata.sh"))
 
 
   # Attach security group to the instances
   network_interfaces {
-     security_groups = [aws_security_group.ejoh-three-tier-ec2-asg-sg.id]
-     associate_public_ip_address = true
+    security_groups             = [aws_security_group.ejoh-three-tier-ec2-asg-sg.id]
+    associate_public_ip_address = true
   }
+  tags = {
+  name = "ejoh-three-tier-web-ec2"
+}
 }
 
 
 
 # Define a Launch Template for EC2 instances
 resource "aws_launch_template" "ejoh-three-tier-app-lconfig" {
-  name_prefix     = "ejoh-three-tier-app-lconfig"
-  image_id        = "ami-0866a3c8686eaeeba"
-  instance_type   = "t2.micro"
-  key_name        = "medium"
-  user_data       = <<-EOF
-                                #!/bin/bash
-
-                                sudo yum install mysql -y
-
-                                EOF
-
+  name_prefix   = "ejoh-three-tier-app-lconfig"
+  image_id      = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  key_name      = "medium"
+  user_data     = base64encode(file("${path.module}/ejohdata1.sh"))
   # Attach security group to the instances
   network_interfaces {
-     security_groups = [aws_security_group.ejoh-three-tier-ec2-asg-sg-app.id]
-     associate_public_ip_address = false
+    security_groups             = [aws_security_group.ejoh-three-tier-ec2-asg-sg-app.id]
+    associate_public_ip_address = false
   }
-  depends_on = [aws_security_group.ejoh-three-tier-ec2-asg-sg-app]
+tags = {
+  name = "ejoh-three-tier-app-ec2"
+}
 }
 
 ##########################################################
